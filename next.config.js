cb849cd029246590ca8aef15792eab137301bf72
the-shoaib2 <the-shoaib2@users.noreply.github.com>
2026-08-12 00:05:03 +0600

    fix: remove force-reset from build script and implement security headers in next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
